ac7d214fe15ffc51e03eafdf477ba3ea1815789d
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-10-14 21:23:05 -0600

    Initialize repo

---
+++ app/src/test/java/com/example/listycity/CityListTest.java
@@ -0,0 +1,65 @@
+package com.example.listycity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class CityListTest {
+    private CityList mockCityList() {
+        CityList cityList = new CityList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+
+    private City mockCity() {
+        return new City("Edmonton", "AB");
+    }
+
+    @Test
+    public void testAdd() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+        City city = new City("Regina", "SK");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+
+    @Test
+    public void testAddException() {
+        CityList cityList = mockCityList();
+        City c = new City("Yellowknife", "NW");
+        cityList.add(c);
+        assertThrows(IllegalArgumentException.class,()->{
+            cityList.add(c);
+        });
+    }
+
+    @Test
+    public void testGetCities() {
+        CityList cityList = mockCityList();
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+
+        City city = new City("Charlottetown", "PE");
+        cityList.add(city);
+
+        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
+    }
+
+    @Test
+    public void testHasCity() {
+        CityList cityList = mockCityList();
+        City edmonton = cityList.getCities().get(0);
+        // Test Edmonton
+        assertTrue(cityList.hasCity(edmonton));
+
+        City vancouver = new City("Vancouver", "BC");
+        cityList.add(vancouver);
+
+        assertTrue(cityList.hasCity(vancouver));
+    }
+}
